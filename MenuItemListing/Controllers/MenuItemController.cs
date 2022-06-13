650f0ec93923f35168774135fd972ba065efd3f8
monishajayasankar <75465736+monishajayasankar@users.noreply.github.com>
2022-06-13 11:07:31 -0700

    Update MenuItemController.cs
    second commit

--- MenuItemListing/Controllers/MenuItemController.cs
+++ MenuItemListing/Controllers/MenuItemController.cs
@@ -16,7 +16,7 @@ namespace MenuItemListing.Controllers
         public List<MenuItem> GetItem()
         {
             List<MenuItem> MenuList = new List<MenuItem>()
-            {   new MenuItem() {Id=1, Name="mobile", Active=true, DateOfLaunch=new DateTime(2022,01,01), FreeDelivery=false,Price=400},
+            {   new MenuItem() {Id=1, Name="Mymobile", Active=true, DateOfLaunch=new DateTime(2022,01,01), FreeDelivery=false,Price=400},
                 new MenuItem() {Id=2, Name="PC", Active=false, DateOfLaunch=new DateTime(2022,10,03), FreeDelivery=false,Price=500}
 
             };
